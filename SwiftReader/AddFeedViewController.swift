dd3a0954d8834de3e29a4bec3395b5f250d051b0
Jason Ellis <palamedes@rocketmail.com>
2015-09-24 10:52:19 -0400

    Test Update..

---
+++ SwiftReader/AddFeedViewController.swift
@@ -0,0 +1,37 @@
+//
+//  AddFeedViewController.swift
+//  SwiftReader
+//
+//  Created by Jason Ellis on 9/24/15.
+//  Copyright © 2015 Jason Ellis. All rights reserved.
+//
+
+import UIKit
+
+class AddFeedViewController: UIViewController {
+
+    @IBOutlet weak var feedUrl: UITextField!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
